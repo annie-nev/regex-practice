b1d877d6796cb39894816faa446a5e62d3180330
Annie <annienabwni46@gmail.com>
2024-03-12 11:36:12 +0200

    finished get_z_row function

--- dot_asterix.py
+++ dot_asterix.py
@@ -5,3 +5,7 @@ def count_lines(the_text):
     lines_list = re.findall("\n", the_text, re.S)
     return len(lines_list)
 
+
+def get_z_row(the_text):
+    the_z_line = re.search(".*Z.*\n", the_text)
+    return the_z_line.group()
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from dot_asterix import count_lines
+from dot_asterix import count_lines, get_z_row
 
 
 def main():
     with open(r'C:\Users\USER\Documents\alice30.txt') as text:
         text_to_be_sent = text.read()
         print(count_lines(text_to_be_sent))
+        print(get_z_row(text_to_be_sent))
 
 
 if __name__ == '__main__':
